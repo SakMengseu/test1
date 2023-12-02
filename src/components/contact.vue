10677f0596ad225401272deea83380e447332a1a
SakMengseu <dinalove4nice@gmail.com>
2023-12-02 17:19:55 +0700

    WebVue1 Test

---
+++ src/components/contact.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="item">
+        <H1>Hello NP</H1>
+        <div class="name">
+            <h1>My Name is Seu</h1>
+            <h1>
+                Age : 20.
+            </h1>
+            <h1>Phone: 097499993</h1>
+        </div>
+    </div>
+</template>
+<style>
+.item .name {
+    color: red;
+    text-align: center;
+}
+
+h1 {
+    font-weight: 700;
+}
+</style>
